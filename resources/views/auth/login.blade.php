f67fd03b810adee16c52dab37efa71e38b51dd6b
danikaharu <dani.kaharu17@gmail.com>
2022-10-22 23:07:07 +0800

    tambah persyaratan di halaman kosong

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,8 +64,107 @@
             </div>
         </div>
 
-        <div class="col-lg-6 d-none d-lg-block">
+        <div class="col-lg-6">
             <div id="auth-right">
+                <h3 class="text-white text-center">Syarat Syarat Kenaikan Pangkat</h3>
+                <div style="color:white;">
+                    <p>Syarat Umum</p>
+                    <ol style="list-style:none;list-style-type: number;">
+                        <li>SK CPNS (Legalisir BKPSDM)</li>
+                        <li>SK PNS (Legalisir BKPSDM)</li>
+                        <li>SK Pangkat Terakhir (Legalisir BKPSDM)</li>
+                        <li>Ijazah terakhir dan transkrip nilai yang (dilegalisir) oleh Kampus atau Sekolah</li>
+                        <li>STTPL bagi yang baru pertama kali mengurus Kenaikan Pangkat (Legalisir BKPSDM)</li>
+                        <li>SK Mutasi bagi yang pernah dimutasikan </li>
+                        <li>SK Pengalihan Jenis Pegawai dari
+                            Badan Kepegawaian Negara bagi Pegawai Negeri Sipil yang pindah antar Provinsi (Legalisir BKPSDM)
+                        </li>
+                        <li>SK Gubernur bagi Pegawai Negeri Sipil yang pindah dari Daerah lain dalam satu Provinsi
+                            (Legalisir BKPSDM)
+                        </li>
+                    </ol>
+                </div>
+                <div style="color:white;">
+                    <p>Kenaikan Pangkat Otomatis atau Reguler</p>
+                    <ol style="list-style:none;list-style-type: number;">
+                        <li>Sasaran Kerja Pegawai (SKP) Tahun 2021 dan 2022 yang dilegalisir oleh Kasubag
+                            Kepegawaian masing-masing SKPD/Kepala Sekolah/Kepala UPTD
+                        </li>
+                    </ol>
+                </div>
+                <div style="color:white;">
+                    <p>Jabatan Struktural</p>
+                    <ol style="list-style:none;list-style-type: number;">
+                        <li>Sertifikat PIM Bagi yang penah mengikuti Diklat (Legalisir BKPSDM)
+                        </li>
+                        <li>Sasaran Kerja Pegawai (SKP) Tahun 2021 dan 2022 yang dilegalisir oleh Kasubag
+                            Kepegawaian masing-masing SKPD/Kepala Sekolah/Kepala UPTD
+                        </li>
+                        <li>SK Jabatan (Legalisir BKPSDM),</li>
+                        <li>Surat Pernyataan Pelantikan (Legalisir BKPSDM)</li>
+                        <li>Surat Menduduki Jabatan Lowong (Legalisir BKPSDM)</li>
+                        <li>Surat Pernyataan Melaksanakan Tugas (Legalisir BKPSDM)</li>
+                        <li>Semua SK Pelantikan yang pernah diduduki (Legalisir BKPSDM)</li>
+                    </ol>
+                </div>
+                <div style="color:white;">
+                    <p>Jabatan Fungsional Tertentu</p>
+                    <ol style="list-style:none;list-style-type: number;">
+                        <li>PAK ASLI (Legalisir)
+                        </li>
+                        <li>PAK LAMA (Legalisir Dikbud)
+                        </li>
+                        <li>SK Jabatan Fungsional (inpassing) bagi yang baru pertama kali mengurus
+                            Kenaikan Pagkat (Legalisir BKPSDM)
+                        </li>
+                        <li>SK Asli Penyesuaian Jabatan Fungsional</li>
+                        <li>SK Asli kenaikan dalam jabatan fungsional</li>
+                    </ol>
+                </div>
+                <div style="color:white;">
+                    <p>Penyesuaian Ijazah (Fungsional Umum)</p>
+                    <ol style="list-style:none;list-style-type: number;">
+                        <li>SK tentang pemberian Tugas Belajar/Ijin Belajar (Legalisir BKPSDM)
+                        </li>
+                        <li>Surat Keterangan Lulus Ujian Dinas (Legalisir BKPSDM)
+                        </li>
+                        <li>Surat Keterangan Perincian Tugas sesuai Tupoksi dan disiplin ilmu yang ditanda tangani oleh
+                            Pejabat Esselon II (Asli)
+                        </li>
+                        <li>Ijazah/transkrip nilai lama sesuai pada SK pangkat terakhir (dilegalisir) oleh kampus
+                            atau sekolah
+                        </li>
+                        <li>Ijazah terakhir/transkrip nilai baru yang (dilegalisir) oleh kampus atau sekolah</li>
+                    </ol>
+                </div>
+                <div style="color:white;">
+                    <p>Penyesuaian Ijazah (Fungsional Tertentu)</p>
+                    <ol style="list-style:none;list-style-type: number;">
+                        <li>PAK ASLI (Legalisir)
+                        </li>
+                        <li>PAK LAMA (Legalisir Dikbud)
+                        </li>
+                        <li>SK Jabatan Fungsional (inpassing) bagi yang baru pertama kali mengurus
+                            Kenaikan Pagkat (Legalisir BKPSDM)
+                        </li>
+                        <li>SK Asli Penyesuaian Jabatan Fungsional
+                        </li>
+                        <li>SK Asli kenaikan dalam jabatan fungsional
+                        </li>
+                        <li>Ijazah/transkrip nilai lama sesuai pada SK pangkat terakhir (dilegalisir) oleh kampus
+                            atau sekolah
+                        </li>
+                        <li>SK tentang pemberian Tugas Belajar/Ijin Belajar (Legalisir BKPSDM)
+                        </li>
+                        <li>Ijazah terakhir/transkrip nilai baru yang (dilegalisir) oleh kampus atau sekolah</li>
+                    </ol>
+                </div>
+                <div style="color:white;">
+                    <p>Pegawai Negeri Sipil usul Kenaikan Pangkat dari Pangkat/Gol. Ruang Pengatur Tkt. I / II D ke
+                        Pangkat/Gol. Ruang Penata Muda / III A melampirkan foto copy sertifikat Ujian Dinas Tingkat I (satu)
+                        atau sertifikasi PIM IV dan Pangkat/Gol. Ruang Penata Tkt. I / III D ke Pangkat/Gol. Ruang Pembina /
+                        IV A melampirkan foto copy sertifikat Ujian Dinas Tingkat II (dua) atau sertifikat PIM II.</p>
+                </div>
             </div>
         </div>
     </div>
