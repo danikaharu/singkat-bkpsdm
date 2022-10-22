14e1a8832c18763f0da15f7f6cee5cd93cf38177
danikaharu <dani.kaharu17@gmail.com>
2022-10-22 23:09:22 +0800

    ganti pencarian berdasarkan nama

--- resources/views/admin/promotion/step2.blade.php
+++ resources/views/admin/promotion/step2.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.admin')
 
-@section('title', trans('Create Promotion'))
+@section('title', trans('Buat Usulan Kenaikan Pangkat'))
+
+@push('css')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+@endpush
 
 @section('content')
     <div class="page-heading">
@@ -36,28 +40,17 @@
                                 <div class="row mb-2">
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label for="nip_baru">{{ __('NIP Baru') }}</label>
-                                            <input type="text" name="nip_baru" id="nip_baru"
-                                                class="form-control @error('nip_baru') is-invalid @enderror"
-                                                placeholder="{{ __('NIP Baru') }}">
-                                            @error('nip_baru')
-                                                <div class="invalid-feedback">
-                                                    {{ $message }}
-                                                </div>
-                                            @enderror
+                                            <label for="name">{{ __('Nama Pegawai') }}</label>
+                                            <select id="selectEmployee" class="form-select" name="name">
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            <label for="nip_lama">{{ __('NIP Lama') }}</label>
-                                            <input type="text" name="nip_lama" id="nip_lama"
-                                                class="form-control @error('nip_lama') is-invalid @enderror"
-                                                placeholder="{{ __('NIP Lama') }}">
-                                            @error('nip_lama')
-                                                <div class="invalid-feedback">
-                                                    {{ $message }}
-                                                </div>
-                                            @enderror
+                                            <label for="nip">{{ __('NIP') }}</label>
+                                            <input type="text" name="nip_baru" id="nip_baru" class="form-control"
+                                                placeholder="{{ __('NIP') }}" readonly>
+
                                         </div>
                                     </div>
 
@@ -67,25 +60,26 @@
                                 <button type="reset" class="btn btn-outline-primary">{{ __('Reset') }}</button>
                             </form>
 
+
                             @if ($employee)
-                                @foreach ($employee as $employee)
+                                @foreach ($employee as $data)
                                     <div class="col-lg-12 mt-5">
-                                        <h2>{{ $employee->nama }}</h2>
+                                        <h2>{{ $data->nama }}</h2>
                                         <table>
                                             <tbody>
                                                 <tr>
-                                                    <td>NIP Baru : {{ $employee->nip_baru }}</td>
+                                                    <td>NIP Baru : {{ $data->nip_baru }}</td>
                                                 </tr>
                                                 <tr>
-                                                    <td>NIP Lama : @if ($employee->nip_baru == $employee->nip)
+                                                    <td>NIP Lama : @if ($data->nip_baru == $data->nip)
                                                             -
                                                         @else
-                                                            {{ $employee->nip }}
+                                                            {{ $data->nip }}
                                                         @endif
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td>Instansi : {{ $employee->agency->n_dinas }}</td>
+                                                    <td>Instansi : {{ $data->agency->n_dinas }}</td>
                                                 </tr>
                                             </tbody>
                                         </table>
@@ -97,7 +91,6 @@
                                         <a class="btn btn-primary" href="{{ route('promotion.step3') }}">Berikutnya</a>
                                     </div>
                                 @endforeach
-                            @else
                             @endif
                         </div>
                     </div>
@@ -106,3 +99,40 @@
         </section>
     </div>
 @endsection
+
+@push('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#selectEmployee').select2({
+                allowClear: true,
+                placeholder: 'Pilih Pegawai',
+                ajax: {
+                    url: "{{ route('employee.select') }}",
+                    delay: 250,
+                    processResults: function({
+                        data
+                    }) {
+                        return {
+                            results: $.map(data, function(item) {
+                                return {
+                                    id: item.nip_baru,
+                                    text: item.nama,
+                                }
+                            })
+                        }
+
+                    }
+                }
+            })
+
+            $('#selectEmployee').change(function() {
+                let name = $('#selectEmployee').val();
+                $('#nip_baru').val(name);
+            })
+        });
+    </script>
+@endpush
