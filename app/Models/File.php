19f416e34fe8ab911abba034c5fcef78023fb742
danikaharu <dani.kaharu17@gmail.com>
2022-10-23 19:44:24 +0800

    add field to files table

--- app/Models/File.php
+++ app/Models/File.php
@@ -9,5 +9,5 @@ class File extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['promotion_id', 'sk_cpns', 'sk_pns', 'sk_pangkat_terakhir', 'kartu_pegawai', 'ijazah_lama', 'ijazah_baru', 'transkrip_lama', 'transkrip_baru', 'skp_lama', 'skp_baru', 'sttpl', 'sk_mutasi', 'sk_pengalihan', 'sk_fungsional', 'pak_asli', 'pak_lama', 'sk_penyesuaian_fungsional', 'sk_penyesuaian_fungsional', 'sertifikat_pim', 'surat_pelantikan', 'surat_lowong', 'surat_tugas', 'sk_pelantikan', 'sk_jabatan', 'sk_belajar'];
+    protected $fillable = ['promotion_id', 'sk_cpns', 'sk_pns', 'sk_pangkat_terakhir', 'kartu_pegawai', 'ijazah_lama', 'ijazah_baru', 'transkrip_lama', 'transkrip_baru', 'skp_lama', 'skp_baru', 'sttpl', 'sk_mutasi', 'sk_pengalihan', 'sk_fungsional', 'pak_asli', 'pak_lama', 'sk_penyesuaian_fungsional', 'sk_penyesuaian_fungsional', 'sertifikat_pim', 'surat_pelantikan', 'surat_lowong', 'surat_tugas', 'sk_pelantikan', 'sk_jabatan', 'sk_belajar', 'uraian_tugas', 'sk_lulus_ujian', 'lainnya'];
 }
