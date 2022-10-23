e0854760061db5e332a9b079536d29b3eabf2c5e
danikaharu <dani.kaharu17@gmail.com>
2022-10-23 20:46:47 +0800

    tambah seeder tabel setting

---
+++ database/seeders/SettingTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::create([
+            'period_1' => 'April',
+            'period_2' => 'Oktober',
+        ]);
+    }
+}
