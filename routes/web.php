7f340d87b4fb645a66dd46bddd14a1451d1b7a84
danikaharu <dani.kaharu17@gmail.com>
2022-12-09 21:45:26 +0800

    tambah route untuk peremajaan dan tambah admin INKA

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::middleware(['auth', 'web'])->prefix('admin')->group(function () {
     Route::post('/promotion/step3', [\App\Http\Controllers\Admin\PromotionController::class, 'storeStep3'])->name('promotion.storeStep3');
     Route::get('/promotion/step4', [\App\Http\Controllers\Admin\PromotionController::class, 'step4'])->name('promotion.step4');
     Route::post('/promotion/verificator/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'storeVerificator'])->name('promotion.storeVerificator');
+    Route::post('/promotion/admin/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'storeAdmin'])->name('promotion.storeAdmin');
     Route::get('/promotion/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'show'])->name('promotion.show');
     Route::delete('/promotion/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'destroy'])->name('promotion.destroy');
     Route::get('/promotion/detail/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'detailApprove'])->name('promotion.detailApprove');
@@ -47,6 +48,7 @@ Route::middleware(['auth', 'web'])->prefix('admin')->group(function () {
     Route::get('promotion/detail/cancel/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'detailCancel'])->name('promotion.detailCancel');
     Route::post('promotion/detail/cancel/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'cancel'])->name('promotion.cancel');
     Route::get('promotion/export/excel', [\App\Http\Controllers\Admin\PromotionController::class, 'exportExcel'])->name('promotion.exportExcel');
+    Route::post('promotion/agree-data/{promotion}', [\App\Http\Controllers\Admin\PromotionController::class, 'agreeData'])->name('promotion.agreeData');
 
     // Employee
     Route::get('/employee', [\App\Http\Controllers\Admin\EmployeeController::class, 'select'])->name('employee.select');
