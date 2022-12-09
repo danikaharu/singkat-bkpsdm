38949a98407fba1bb088639ef238423ddb1e2015
danikaharu <dani.kaharu17@gmail.com>
2022-12-09 21:43:26 +0800

    tambah relasi unit dan field unit_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+      	'unit_id',
         'name',
         'username',
         'email',
@@ -43,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+  
+  	 public function unit()
+    {
+        return $this->belongsTo(Unit::class, 'unit_id', 'k_unor');
+    }
 }
